7b75b1d529fb95071de88ee370530bdd702c0cea
Ragingfury6 <trentsuperman@gmail.com>
2026-01-24 16:29:02 -0700

    feat: implement responsive navigation menu with toggle functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/NavBar.css';
 import crest from '../images/Sigma_Chi_Crest.png'; 
 
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <nav className="navbar navbar-dark" id="navbar">
             <div className="nav-container">
                 <Link to="/" className="logo">
                     <img src={crest} alt="Sigma Chi Crest" className="logo-img" />
                 </Link>
-                <ul className="nav-menu">
+                <div className={`hamburger ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
+                <ul className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
                     <li><Link to="/about" className="nav-link">About</Link></li>
                     <li><Link to="/class" className="nav-link">Class</Link></li>
                     <li><Link to="/experience" className="nav-link">Experience</Link></li>
@@ -22,4 +34,4 @@ function Navbar() {
     )
 }
 
-export default Navbar
+export default Navbar;
